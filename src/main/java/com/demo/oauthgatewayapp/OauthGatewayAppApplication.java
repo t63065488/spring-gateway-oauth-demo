420de8f03ca436246e8c5709a80e3dea8b746c96
t63065488 <t63065488@gmail.com>
2024-01-04 19:32:33 +0000

    Initial commit

---
+++ src/main/java/com/demo/oauthgatewayapp/OauthGatewayAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.oauthgatewayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthGatewayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthGatewayAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/oauthgatewayapp/OauthGatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.oauthgatewayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthGatewayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
